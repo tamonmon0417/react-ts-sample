9a141ad7aeddf7d336a5dcb780ed47f12f9f7948
tamonmon0417 <tamonmon0417@gmail.com>
2021-10-14 22:59:03 +0900

    add reg

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom'
 import EventComponent from './events/EventComponents'
 // import GuestList from './state/GuestList'
-import UserSearch from './state/UserSearch'
+import UserSearch from './refs/UserSearch'
 
 
 const App = () => {
   return <div>
     {/* <GuestList></GuestList> */}
-    {/* <UserSearch></UserSearch> */}
-    <EventComponent></EventComponent>
+    <UserSearch></UserSearch>
+    {/* <EventComponent></EventComponent> */}
   </div>
 }
 
